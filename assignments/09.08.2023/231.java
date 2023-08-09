21b3100719b43acd0dfe328078585351ab6d959b
Meet <meetsuthar2005@gmail.com>
2023-08-09 20:18:51 +0530

    9th August Submission

---
+++ assignments/09.08.2023/231.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+        if (n == 0) {
+            return false;
+        }
+
+        while (n != 0) {
+            if (n == 1) {
+                return true;
+            }
+
+            if (n % 2 != 0) {
+                return false;
+            }
+
+            else {
+                n = n / 2;
+            }
+        }
+
+        return true;
+
+    }
+}
