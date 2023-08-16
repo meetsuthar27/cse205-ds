07447dcaeb80951f6f9951daa06601674ccff87a
Meet <meetsuthar2005@gmail.com>
2023-08-16 14:55:53 +0530

    Subsets solution with - Recursion

--- assignments/09.08.2023/78.java
+++ assignments/09.08.2023/78.java
@@ -1,20 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         helper(nums, 0, new ArrayList<>(), result);
         return result;
     }
-
-    void helper(int[] nums, int start, List<Integer> subset, List<List<Integer>> result) {
-        result.add(new ArrayList<>(subset));
-
-        for (int i = start; i < nums.length; i++) {
-            subset.add(nums[i]);
-            helper(nums, i + 1, subset, result);
-            subset.remove(subset.size() - 1);
+    
+    void helper(int[] nums, int index, List<Integer> subset, List<List<Integer>> result) {
+        if (index == nums.length) {
+            result.add(new ArrayList<>(subset));
+            return;
         }
+        
+        // Exclude
+        helper(nums, index + 1, subset, result);
+        
+        // Include 
+        subset.add(nums[index]);
+        helper(nums, index + 1, subset, result);
+        subset.remove(subset.size() - 1);
     }
 }
