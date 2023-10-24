27091c521ed4798adb8ef09ef7d878e50933ccb0
Meet Suthar <120668344+meetsuthar27@users.noreply.github.com>
2023-10-25 00:25:46 +0530

    18th October | Submissions

---
+++ assignments/18.10.2023/104.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int ans;
+        
+        if(!root)
+        {
+            return 0;
+        }
+
+        int ml=maxDepth(root->left);
+        int mr= maxDepth(root->right);
+
+        ans = max(ml,mr)+1;
+
+        return ans;
+        
+    }
+};
